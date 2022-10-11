35816d66c25ee4e00f22de8f86007aed46862792
Ragnazar <avdeyev.iv@gmail.com>
2022-10-11 17:42:15 +0300

    add-avatar-controller

--- src/main/java/ru/hogwarts/school/controller/AvatarController.java
+++ src/main/java/ru/hogwarts/school/controller/AvatarController.java
@@ -1,9 +1,20 @@
 package ru.hogwarts.school.controller;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import ru.hogwarts.school.model.Avatar;
 import ru.hogwarts.school.service.AvatarService;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 @RestController
 @RequestMapping("avatar")
@@ -14,26 +25,36 @@ public class AvatarController {
         this.avatarService = avatarService;
     }
 
-    @PostMapping
-    public Avatar uploadAvatar(@RequestBody Avatar avatar) {
-        return avatarService.saveAvatar(avatar);
-    }
-
-    @GetMapping(path = "{id}/local")
-    public ResponseEntity<Avatar> getAvatarFromLocal(@PathVariable Long id) {
-        Avatar avatar = avatarService.findAvatar(id);
-        if (avatar == null) {
-            return ResponseEntity.notFound().build();
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<String> uploadAvatar(@RequestParam MultipartFile avatar) throws IOException {
+        if (avatar.getSize() >= 1024 * 300) {
+            return ResponseEntity.badRequest().body("File is too big");
         }
-        return ResponseEntity.ok(avatar);
-    }
+            avatarService.saveAvatar(avatar);
 
-    @GetMapping(path = "{id}/source")
-    public ResponseEntity<Avatar> getAvatarFromSource(@PathVariable Long id) {
-        Avatar avatar = avatarService.findAvatar(id);
-        if (avatar == null) {
-            return ResponseEntity.notFound().build();
-        }
-        return ResponseEntity.ok(avatar);
+            return ResponseEntity.ok().build();
+
+
     }
-}
+        @GetMapping(value = "{id}/avatar-from-db")
+        public ResponseEntity<byte[]> downloadAvatar (@PathVariable Long id){
+            Avatar result = avatarService.findAvatar(id);
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.parseMediaType(result.getMediaType()));
+            headers.setContentLength(result.getData().length);
+            return ResponseEntity.status(HttpStatus.OK).headers(headers).body(result.getData());
+        }
+
+        @GetMapping(value = "/{id}/avatar-from-file")
+        public void downloadAvatar (@PathVariable Long id, HttpServletResponse response) throws IOException {
+            Avatar avatar = avatarService.findAvatar(id);
+            Path path = Path.of(avatar.getFilePath());
+            try (InputStream is = Files.newInputStream(path);
+                 OutputStream os = response.getOutputStream()) {
+                response.setStatus(200);
+                response.setContentType(avatar.getMediaType());
+                response.setContentLength((int) avatar.getFileSize());
+                is.transferTo(os);
+            }
+        }
+    }
--- src/main/java/ru/hogwarts/school/service/AvatarService.java
+++ src/main/java/ru/hogwarts/school/service/AvatarService.java
@@ -1,26 +1,56 @@
 package ru.hogwarts.school.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 import ru.hogwarts.school.model.Avatar;
 import ru.hogwarts.school.repository.AvatarRepository;
 
 import javax.transaction.Transactional;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Collection;
 
+import static java.nio.file.StandardOpenOption.CREATE_NEW;
+
 @Service
+@Transactional
 public class AvatarService {
+    @Value("${student.avatar.dir.path}")
+    private String avatarsDir;
+
+    private final StudentService studentService;
     private final AvatarRepository avatarRepository;
 
-    public AvatarService(AvatarRepository avatarRepository) {
+    public AvatarService(AvatarRepository avatarRepository, StudentService studentService) {
         this.avatarRepository = avatarRepository;
+        this.studentService = studentService;
     }
 
-    public Avatar saveAvatar(Avatar avatar){
-        return avatarRepository.save(avatar);
+    public void saveAvatar(MultipartFile file) throws IOException {
+        Path filePath = Path.of(avatarsDir, file.getOriginalFilename());
+        Files.createDirectories(filePath.getParent());
+        Files.deleteIfExists(filePath);
+
+        try (InputStream is = file.getInputStream();
+             OutputStream os = Files.newOutputStream(filePath, CREATE_NEW);
+             BufferedInputStream bis = new BufferedInputStream(is, 1024);
+             BufferedOutputStream bos = new BufferedOutputStream(os, 1024)
+        ) {
+            bis.transferTo(bos);
+        }
+
+        Avatar avatar = new Avatar();
+        avatar.setFilePath(filePath.toString());
+        avatar.setFileSize(file.getSize());
+        avatar.setMediaType(file.getContentType());
+
+        avatarRepository.save(avatar);
     }
 
-    public Avatar findAvatar(Long id){
-        return avatarRepository.findById(id).orElse(null);
+    public Avatar findAvatar(Long id) {
+        return avatarRepository.findById(id).orElse(new Avatar());
     }
 
     @Transactional
@@ -28,7 +58,7 @@ public class AvatarService {
         return avatarRepository.findAll();
     }
 
-    public Avatar editAvatar(Avatar avatar){
+    public Avatar editAvatar(Avatar avatar) {
         return avatarRepository.save(avatar);
     }
 
